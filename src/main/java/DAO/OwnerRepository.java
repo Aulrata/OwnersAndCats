0f9d60429a44b650b98654bdbb8c08bf85fc2fe8
Aulrata <Aulrata@gamil.com>
2024-03-24 13:59:35 +0300

    feat: added crud methods

--- src/main/java/DAO/OwnerRepository.java
+++ src/main/java/DAO/OwnerRepository.java
@@ -31,4 +31,49 @@ public class OwnerRepository
             entityManagerFactory.close();
         }
     }
+
+    public Owner FindById(Integer _id)
+    {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        Owner owner = null;
+        try
+        {
+            transaction.begin();
+            owner = entityManager.find(Owner.class, _id);
+            transaction.commit();
+        }
+        catch (Exception e) {
+            if (transaction.isActive()) transaction.rollback();
+            e.printStackTrace();
+        }
+        finally {
+            entityManager.close();
+            entityManagerFactory.close();
+        }
+        return owner;
+    }
+
+    public void Update(Owner owner)
+    {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        try
+        {
+            transaction.begin();
+            entityManager.merge(owner);
+            transaction.commit();
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
+        finally
+        {
+            entityManager.close();
+            entityManagerFactory.close();
+        }
+    }
 }
